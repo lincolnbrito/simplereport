085d44188a1349ecc3cd2c61b918bbb9278269d6
andersonsouzars@gmail.com <andersonsouzars@gmail.com@31437d5a-b23a-ae7b-deaa-8627a19c55ad>
2012-11-15 02:31:30 +0000

    estrutura

--- trunk/index.php
+++ trunk/index.php
@@ -2,10 +2,14 @@
 require_once 'XmlLoader.php';
 require_once 'SimpleReport.php';
 require_once 'SimpleSerializeManager.php';
+require_once 'MontaRelatorio.php';
 
-$design = XmlLoader::load('report.jrxml'); 
-
-$report = SimpleSerializeMananger::compileReport($design);
-
-
+if(file_exists('report.sr')){
+	$design = unserialize(file_get_contents('report.sr'));
+}else{
+	$design = XmlLoader::load('report.jrxml');
+}
+ 
+$print = MontaRelatorio::desenha($design);
+$print->imprimir();
 ?>
