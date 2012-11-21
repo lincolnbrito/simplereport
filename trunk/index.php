e6f8160925dbc8cab5bdf6500da2652b08b2a648
andersonsouzars@gmail.com <andersonsouzars@gmail.com@31437d5a-b23a-ae7b-deaa-8627a19c55ad>
2012-11-21 00:01:32 +0000

    estruturando

--- trunk/index.php
+++ trunk/index.php
@@ -20,7 +20,15 @@ $design = SRXmlLoader::load('report.jrxml');
 
 $report = SRCompileManager::compile($design);
 
+
+$conexao = mysql_connect('localhost', 'root', 'root');
+$db_selected = mysql_select_db('buscaoffshore', $conexao);
+$consulta = mysql_query('select * from empresas', $conexao);
+$dados = array();
+while($r = mysql_fetch_assoc($consulta)){
+	$dados[] = array($r['id'], $r['nome']);
+}
+
+
 SRFillManager::fillReport($report)->show();
-
-
 ?>
