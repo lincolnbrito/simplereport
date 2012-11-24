d97de149625f1a9423e0387b6e3ad48495c03892
andersonsouzars@gmail.com <andersonsouzars@gmail.com@31437d5a-b23a-ae7b-deaa-8627a19c55ad>
2012-11-24 04:11:12 +0000

    Foi criado os elementos Image e Rectangle. E agora esta com a possibilidade de pintar os StaticText

--- trunk/simpleReport/elements/StaticText.php
+++ trunk/simpleReport/elements/StaticText.php
@@ -5,10 +5,17 @@ require_once 'simpleReport/core/SRElements.php';
 class StaticText extends SRElements{
 
 	public $text;
-		
+	public $paintBackground;
+	
 	public function draw(&$pdf){
+		if(!empty($this->forecolor)){
+			$pdf->SetTextColor($this->forecolor[0],$this->forecolor[1],$this->forecolor[2]);
+		}
+		if(!empty($this->backcolor)){
+			$pdf->SetFillColor($this->backcolor[0],$this->backcolor[1],$this->backcolor[2]);
+		}
 		$pdf->SetXY($this->x,$this->y);
-		$pdf->Cell($this->width,$this->height,$this->text);
+		$pdf->Cell($this->width,$this->height,$this->text, 0, 0, 'L', $this->paintBackground);
 	}
 	
 }
